9162bc00a073afe0ff47cc9f5216c5637f811d2a
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-04-03 11:15:16 +0200

    created arrays with random advices

---
+++ script.js
@@ -0,0 +1,21 @@
+function generateRandomNumber(num) {
+  // Gets # from 0 -> num - 1
+  return Math.floor(Math.random() * num);
+}
+
+const codingAdvicePackage = {
+  codingLanguage: ["Javascript", "Typescript", "Python", "Swift", "Java"],
+  codingLevel: [
+    "professional",
+    "beginner",
+    "intermediate",
+    "leader",
+    "human with super power",
+  ],
+  codingAdvice: [
+    "sleep",
+    "eat, learn and repeat",
+    "ask questions",
+    "study at WBS Coding School",
+  ],
+};
