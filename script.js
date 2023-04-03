7ab24f2bdeec8af438a50f7ab1d089fa3352f70c
Kristof Eilers <127109500+KristofEilers@users.noreply.github.com>
2023-04-03 12:12:15 +0200

    added switch loop

--- script.js
+++ script.js
@@ -19,3 +19,31 @@ const codingAdvicePackage = {
     "study at WBS Coding School",
   ],
 };
+
+let personalAdvice = [];
+
+for (let prop in personalAdvice) {
+  let optionIdx = generateRandomNumber(personalAdvice[prop].length);
+
+  switch (prop) {
+    case "codingLanguage":
+      personalAdvice.push(
+        `Your coding language of the day is "${codingAdvicePackage[prop][optionIdx]}".`
+      );
+      break;
+    case "codingLevel":
+      personalAdvice.push(
+        `This is your chance to become a "${codingAdvicePackage[prop][optionIdx]}".`
+      );
+      break;
+    case "codingAdvice":
+      personalAdvice.push(
+        `You only have to "${codingAdvicePackage[prop][optionIdx]}".`
+      );
+      break;
+    default:
+      personalAdvice.push(
+        "OMG - There is not enough info. You should better become an Excel-Expert!"
+      );
+  }
+}
